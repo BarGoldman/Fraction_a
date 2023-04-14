e4247dbed157107c419be0b15282df2d8c8bf9e8
BarGoldman <93201414+BarGoldman@users.noreply.github.com>
2023-04-14 11:18:30 +0000

    temp

--- sources/Fraction.hpp
+++ sources/Fraction.hpp
@@ -7,70 +7,78 @@ namespace ariel
 class Fraction
 {
 private:
-    double m_l;
-    double m_r;
+    double _numerator;
+    double _denominator;
 
 public:
-    Fraction(const double &ml,const double &mr);
+    // constructor
+    Fraction(const double &ml, const double &mr);
     ~Fraction();
+
+
     Fraction operator+(const Fraction &other) const;
     Fraction operator-(const Fraction &other) const;
     Fraction operator/(const Fraction &other) const;
-    Fraction operator*(const Fraction &other) const;
-    // Fraction operator<<(const Fraction &other) const;
-    double m_l() const
-    {
-        return m_l;
-    }
+    Fraction operator*(const Fraction &other) const; // a * b
 
-    double m_r() const
-    {
-        return m_r;
-    }
-
-    Fraction &operator--()
-    {
-        m_l -= m_r;
-        return *this;
-    }
-
-    Fraction operator--(int)
-    {
-        Fraction tmp(*this);
-        --(*this);
-        return tmp;
-    }
-
-    Fraction &operator++()
-    {
-        m_l += m_r;
-        return *this;
-    }
-
-    Fraction operator++(int)
-    {
-        Fraction tmp(*this);
-        ++(*this);
-        return tmp;
-    }
-
-    // Fraction operator--()
-    // {
-    //     Fraction copy = *this;
-    //     m_l--;
-    //     return copy;
-    // }
-
-    // Fraction &operator--()
-    // {
-    //     m_l -= m_r;
-    //     return *this;
-    // }
+    // overloading equality operators 
 
 
-    // 26
-    friend Fraction operator*(const Fraction &c1, const Fraction &c2);
+//     Fraction operator*(double num) const;
+//     // Fraction operator<<(const Fraction &other) const;
+//     double m_l() const
+//     {
+//         return m_l;
+//     }
+
+//     double m_r() const
+//     {
+//         return m_r;
+//     }
+
+//     Fraction &operator--()
+//     {
+//         m_l -= m_r;
+//         return *this;
+//     }
+
+//     Fraction operator--(int)
+//     {
+//         Fraction tmp(*this);
+//         --(*this);
+//         return tmp;
+//     }
+
+//     Fraction &operator++()
+//     {
+//         m_l += m_r;
+//         return *this;
+//     }
+
+//     Fraction operator++(int)
+//     {
+//         Fraction tmp(*this);
+//         ++(*this);
+//         return tmp;
+//     }
+
+//     // Fraction operator--()
+//     // {
+//     //     Fraction copy = *this;
+//     //     m_l--;
+//     //     return copy;
+//     // }
+
+//     // Fraction &operator--()
+//     // {
+//     //     m_l -= m_r;
+//     //     return *this;
+//     // }
+
+
+//     // 26
+    friend Fraction operator*(Fraction &other1, const Fraction &other2); // 2.3*b
     friend std::ostream &operator<<(std::ostream &output, const Fraction &c);
-    friend bool operator>(const Fraction& c1, const Fraction& c2);
-//    bool operator>=(const Fraction& other);
+//     friend bool operator>(const Fraction& c1, const Fraction& c2);
+//    bool operator>=(const Fraction &other) const;
 };
