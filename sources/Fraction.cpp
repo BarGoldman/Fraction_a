9911affb8632b379827bdefcf0eb8433f72b9966
BarGoldman <93201414+BarGoldman@users.noreply.github.com>
2023-04-14 15:30:12 +0000

    fix

--- sources/Fraction.cpp
+++ sources/Fraction.cpp
@@ -1,68 +1,90 @@
 #include "Fraction.hpp"
 
 namespace ariel
-{};
-
-Fraction operator+(const Fraction &other) const;
-    friend Fraction operator+(const Fraction &other, float num);
-    friend Fraction operator+(float num, const Fraction &other);
-
-    // The - operator to subtract two fractions and return their difference as another fraction in reduced form.
-    Fraction operator-(const Fraction &other) const;
-    friend Fraction operator-(const Fraction &other, float num);
-    friend Fraction operator-(float num, const Fraction &other);
-
-    // The * operator to multiply two fractions and return their product as another fraction in reduced form.
-    Fraction operator*(const Fraction &other) const; // a * b
-    friend Fraction operator*(const Fraction &other, float num);
-    friend Fraction operator*(float num, const Fraction &other);
-
-    // The / operator to divide two fractions and return their quotient as another fraction in reduced form.
-    Fraction operator/(const Fraction &other) const;
-    friend Fraction operator/(const Fraction &other, float num);
-    friend Fraction operator/(float num, const Fraction &other);
+{
+};
 
 
+// The + operator to add two fractions and return their sum as another fraction in reduced form.
+Fraction Fraction::operator+(const Fraction& other) const
+{
+    return Fraction(_numerator + other._numerator,  _denominator + other._denominator); 
+}
+Fraction operator+(const Fraction& other, float num){
+    float temp = other._numerator / other._denominator + num;
+    return Fraction(temp);
+}
+Fraction operator+(float num, const Fraction& other){
 
+}
 
-    //The == operator to compare two fractions for equality and return true or false.
-    bool operator==(const Fraction &other) const;
-    friend bool operator==(const Fraction &other, float num);
-    friend bool operator==(float num, const Fraction &other);
+// The - operator to subtract two fractions and return their difference as another fraction in reduced form.
+Fraction Fraction::operator-(const Fraction& other) const{
+    return Fraction(_numerator - other._numerator,  _denominator - other._denominator);
+}
+Fraction operator-(const Fraction& other, float num);
+Fraction operator-(float num, const Fraction& other);
 
-    // All comparison operations (>,<,>=,<=)
-    //  >
-    bool operator>(const Fraction &other) const;
-    friend bool operator>(const Fraction &other, float num);
-    friend bool operator>(float num, const Fraction &other);
+// The * operator to multiply two fractions and return their product as another fraction in reduced form.
+Fraction Fraction::operator*(const Fraction& other) const{
+    return Fraction(_numerator * other._numerator,  _denominator * other._denominator);
+}
+Fraction operator*(const Fraction& other, float num);
+Fraction operator*(float num, const Fraction& other);
 
-    // <
-    bool operator<(const Fraction &other) const;
-    friend bool operator<(const Fraction &other, float num);
-    friend bool operator<(float num, const Fraction &other);
+// The / operator to divide two fractions and return their quotient as another fraction in reduced form.
+Fraction Fraction::operator/(const Fraction& other) const{
+    return Fraction(_numerator / other._numerator,  _denominator / other._denominator);
+}
+Fraction operator/(const Fraction& other, float num);
+Fraction operator/(float num, const Fraction& other);
 
-    // >=
-    bool operator>=(const Fraction &other) const;
-    friend bool operator>=(const Fraction &other, float num);
-    bool operator>=(float num, const Fraction &other);
+// The == operator to compare two fractions for equality and return true or false.
+bool operator==(const Fraction& other) const;
+friend bool operator==(const Fraction& other, float num);
+friend bool operator==(float num, const Fraction& other);
 
-    // <=
-    bool operator<=(const Fraction &other){
-        return true;
-    }   
-    bool operator<=(const Fraction &other, float num){
-        return true;
-    }
-    bool operator<=(float num, const Fraction &other){
-        return true;
-    }
+// All comparison operations (>,<,>=,<=)
+//  >
+bool operator>(const Fraction& other) const;
+friend bool operator>(const Fraction& other, float num);
+friend bool operator>(float num, const Fraction& other);
 
-    // The << operator to print a fraction to an output stream in the format “numerator/denominator”.
-    ostream &operator<<(ostream &output, const Fraction &other){
-        return output;
-    }
+// <
+bool operator<(const Fraction& other) const;
+friend bool operator<(const Fraction& other, float num);
+friend bool operator<(float num, const Fraction& other);
 
-    // The >> operator to read a fraction from an input stream by taking two integers as input.
-    istream &operator>>(istream &input, const Fraction &other){
-        return input;
-    }
+// >=
+bool operator>=(const Fraction& other) const;
+bool operator>=(const Fraction& other, float num)
+{
+}
+bool operator>=(float num, const Fraction& other)
+{
+}
+
+// <=
+bool operator<=(const Fraction& other)
+{
+}
+bool operator<=(const Fraction& other, float num)
+{
+    return true;
+}
+bool operator<=(float num, const Fraction& other)
+{
+    return true;
+}
+
+// The << operator to print a fraction to an output stream in the format “numerator/denominator”.
+ostream& operator<<(ostream& output, const Fraction& other)
+{
+    return (output << other._numerator << '/' << other._denominator);
+}
+
+// The >> operator to read a fraction from an input stream by taking two integers as input.
+istream& operator>>(istream& input, const Fraction& other)
+{
+    return input;
+}
