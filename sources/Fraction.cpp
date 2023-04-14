d0b9670e3ed8a9eafa173f6125fbfe1c5a66e5f0
BarGoldman <93201414+BarGoldman@users.noreply.github.com>
2023-04-14 10:48:53 +0000

    temp

---
+++ sources/Fraction.cpp
@@ -0,0 +1,13 @@
+#include "Fraction.hpp"
+
+namespace ariel
+{};
+
+    Fraction::Fraction(const double &ml ,
+                       const double &mr) : m_l(ml), m_r(mr)
+    {
+    }
+
+    Fraction::~Fraction()
+    {
+    }
--- sources/Fraction.hpp
+++ sources/Fraction.hpp
@@ -9,17 +9,68 @@ class Fraction
 private:
     double m_l;
     double m_r;
+
 public:
-    Fraction(const double& ml= 0.0,
-             const double& mr= 0.0);
+    Fraction(const double &ml,const double &mr);
     ~Fraction();
+    Fraction operator+(const Fraction &other) const;
+    Fraction operator-(const Fraction &other) const;
+    Fraction operator/(const Fraction &other) const;
+    Fraction operator*(const Fraction &other) const;
+    // Fraction operator<<(const Fraction &other) const;
+    double m_l() const
+    {
+        return m_l;
+    }
+
+    double m_r() const
+    {
+        return m_r;
+    }
+
+    Fraction &operator--()
+    {
+        m_l -= m_r;
+        return *this;
+    }
+
+    Fraction operator--(int)
+    {
+        Fraction tmp(*this);
+        --(*this);
+        return tmp;
+    }
+
+    Fraction &operator++()
+    {
+        m_l += m_r;
+        return *this;
+    }
+
+    Fraction operator++(int)
+    {
+        Fraction tmp(*this);
+        ++(*this);
+        return tmp;
+    }
+
+    // Fraction operator--()
+    // {
+    //     Fraction copy = *this;
+    //     m_l--;
+    //     return copy;
+    // }
+
+    // Fraction &operator--()
+    // {
+    //     m_l -= m_r;
+    //     return *this;
+    // }
+
+
+    // 26
+    friend Fraction operator*(const Fraction &c1, const Fraction &c2);
+    friend std::ostream &operator<<(std::ostream &output, const Fraction &c);
+    friend bool operator>(const Fraction& c1, const Fraction& c2);
+//    bool operator>=(const Fraction& other);
 };
-
-Fraction::Fraction(const double& ml= 0.0,
-             const double& mr= 0.0):m_l(ml) , m_r(mr)
-{
-}
-
-Fraction::~Fraction()
-{
-}
